b8418bdf0917722fa685d0961b01331f349100ca
melih özdil <melih@melih-MacBook-Air.local>
2022-04-13 17:51:05 +0300

    first commit

---
+++ components/ManageExpense/ExpenseForm.js
@@ -0,0 +1,61 @@
+import { View, Text, StyleSheet } from "react-native";
+
+import Input from "./Input";
+
+function ExpenseForm() {
+  function amountChangeHandler() {}
+  return (
+    <View style={styles.form}>
+      <Text style={styles.title}>Your Expenses</Text>
+      <View style={styles.inputsRow}>
+        <Input
+          style={styles.rowInput}
+          label="Amount"
+          textInputConfig={{
+            keyboardType: "decimal-pad",
+            onChangeText: amountChangeHandler,
+          }}
+        />
+        <Input
+          style={styles.rowInput}
+          label="Date"
+          textInputConfig={{
+            placeholder: "YYYY-MM-DD",
+            maxLength: 10,
+            onChangeText: () => {},
+          }}
+        />
+      </View>
+      <Input
+        label="Description"
+        textInputConfig={{
+          multiline: true,
+          // autoCapitalize: none
+          // autocorrect: false, // default is true
+        }}
+      />
+    </View>
+  );
+}
+
+export default ExpenseForm;
+
+const styles = StyleSheet.create({
+  form: {
+    marginTop: 80,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "white",
+    marginVertical: 24,
+  },
+  inputsRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  rowInput: {
+    flex: 1,
+  },
+});
